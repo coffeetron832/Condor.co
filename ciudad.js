77ebb3352aa6781c80e693189ec2c8361ac90c0c
coffeetron832 <ju4nhponton@gmail.com>
2026-08-12 16:57:55 -0500

    Update ciudad.js

--- ciudad.js
+++ ciudad.js
@@ -1,3 +1,21 @@
+/*
+ * 24col - Directorio Ciudadano Colombiano
+ * Copyright (C) 2026 jahp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
     const cityParam = urlParams.get('city') || 'bogota';
